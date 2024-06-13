cdfbd493e0b4f733c7cb725e543a171606134be6
Kenan Kadic <ken.kadic@outlook.com>
2024-06-13 15:41:46 -0500

    Update dcm4che_storescu.sh shortened and removed extra comments

--- scripts/dcm4che_storescu.sh
+++ scripts/dcm4che_storescu.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 
-# Script determines the location of the DCM4CHE storescu command
-# First version just looked for it on the users PATH, but that
-# did not work when users also had installed DCMTK which has
-# it's own version with the same name.
-
-
-# This function of the meat of the script
-# I commented out the original code that looked on the user's PATH
-# As mentioned above, that fails when DCMTK is installed
-# and the DCMTK storescu is on the PATH before the DCM4CHE version
+# This script finds the location of the DCM4CHE version of the storescu command
 
 find_storescu() {
-# storescu_path=`which storescu`
-# if [[ $? -ne 0 ]] ; then
-#  if [ -z ${DCM4CHE_BASE+x} ] ; then
-#   # Variable is not set
-#   storescu_path=`which storescu`
-#  else
-#   # Variable is set
-#   storescu_path="$DCM4CHE_BASE/bin/storescu"
-#  fi
-# fi
-
- storescu_path="$DCM4CHE_BASE/bin/storescu"
- echo "$storescu_path"
+storescu_path="$DCM4CHE_BASE/bin/storescu"
+echo "$storescu_path"
 }
 
 export DCM4CHE_STORESCU=$(find_storescu)
