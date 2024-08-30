dd1d05a477a345cfb8651b045096d21dcb1fc2a2
Steve Moore <moore.stephen.m@wustl.edu>
2024-08-30 08:17:10 -0500

    send_to_backup: Add options to set Patient Name and/or Paitent ID

--- scripts/ccir_send_to_backup.sh
+++ scripts/ccir_send_to_backup.sh
@@ -2,7 +2,7 @@
 
 # Function to display usage instructions
 usage() {
-  echo "Usage: $0 [-s subject] [-h host] directory"
+  echo "Usage: $0 [-s subject] [-n patient name] [-i patient id] [-h host] directory"
   exit 1
 }
 
@@ -12,13 +12,21 @@ source $SCRIPTS/dcm4che_storescu.sh
 
 # Initialize variables
 subject=""
+patient_name=""
+patient_id=""
 host="cnda-shadow01.nrg.wustl.edu" # Default value for the host
 
 # Parse command-line options
-while getopts "s:h:" opt; do
+while getopts "s:n:i:h:" opt; do
   case $opt in
     s)
       subject=$OPTARG
+      ;;
+    n)
+      patient_name=$OPTARG
+      ;;
+    i)
+      patient_id=$OPTARG
       ;;
     h)
       host=$OPTARG
@@ -47,13 +55,23 @@ directory=$1
 # Build the storescu command using the DCM4CHE_STORESCU variable
 storescu_cmd="$DCM4CHE_STORESCU -b CCIR-CTP -c CNDA_CCIR@$host:8104"
 
+# Add the directory to the storescu command
+storescu_cmd="$storescu_cmd $directory"
+
 # If subject is provided, add the -s option for StudyDescription
 if [ -n "$subject" ]; then
   storescu_cmd="$storescu_cmd -s 00081030=$subject"
 fi
 
-# Add the directory to the storescu command
-storescu_cmd="$storescu_cmd $directory"
+# If patient_name is provided, add the -s option for PatientName
+if [ -n "$patient_name" ]; then
+  storescu_cmd="$storescu_cmd -s 00100010=$patient_name"
+fi
+
+# If patient_id is provided, add the -s option for PatientName
+if [ -n "$patient_id" ]; then
+  storescu_cmd="$storescu_cmd -s 00100020=$patient_id"
+fi
 
 # Print the constructed command for debugging
 echo "Executing command: $storescu_cmd"
