07229f8ee0a7a2ce1c66861ee957b4f34868f0a9
Jenny Gurney <gkgurney@wustl.edu>
2024-09-16 09:30:03 -0500

    update to index.py and test_prearchive_index.sh

--- python/test/test_prearchive_index.sh
+++ python/test/test_prearchive_index.sh
@@ -2,5 +2,27 @@
 
 export PYTHONPATH=/data/CNDA/home/helper_scripts/python/src
 
-python3 -m prearchive.index -M -D -c /tmp/prearchive.csv /data/CNDA/prearchive
+# Check if the first argument (-t) for Top Level Folders is provided
+if [ -z "$1" ]; then
+    # Prompt user with a yes/no question
+    echo "Warning: No top-level folder names provided. If you continue, the script will scan the entire prearchive."
+    read -p "Do you want to continue? (yes/no): " answer
+
+    case "$answer" in 
+        [Yy]* ) 
+            echo "Continuing to scan the entire prearchive..."
+            ;;
+        [Nn]* ) 
+            echo "Exiting script."
+            exit 1
+            ;;
+        * ) 
+            echo "Invalid input. Exiting script."
+            exit 1
+            ;;
+    esac
+fi
+
+# Run the Python script, using the provided top-level folders if given
+python3 -m prearchive.index -M -D -c /tmp/prearchive.csv -l /tmp/log_of_prearchive.txt -t "$1" /data/CNDA/prearchive
 
